cf3ce5ecb8f2b92f635695ebc7bb98568f37b071
Abdelrahman Gamal <abdalrahmangamal449@gmail.com>
2026-02-26 16:38:20 +0200

    sdsd

--- routes/api.php
+++ routes/api.php
@@ -2,17 +2,16 @@
 
 
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\PostController;
-use App\Http\Controllers\CommentController;
-use App\Http\Controllers\TagController;
-
-Route::post("/blog",[PostController::class,'create']);
-Route::delete("/blog/{id}",[PostController::class,'delete']);
-
-
-
-Route::post("/comments",[CommentController::class,'create']);
+// use App\Http\Controllers\PostController;
+// use App\Http\Controllers\CommentController;
+// use App\Http\Controllers\TagController;
+use App\Http\Controllers\api\PostApiController;
+// Route::post("/blog",[PostController::class,'create']);
+// Route::delete("/blog/{id}",[PostController::class,'delete']);
+// Route::post("/comments",[CommentController::class,'create']);
 
 
 
-Route::post("/tag",[TagController::class,'create']);
+// Route::post("/tag",[TagController::class,'create']);
+
+Route::apiResource("/blog",PostApiController::class);
